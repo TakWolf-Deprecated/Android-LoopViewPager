5d4203c1d89c00c7f69f350fb2b4d090979b2c71
TakWolf <takwolf@foxmail.com>
2022-01-21 21:14:04 +0800

    基本项目结构

---
+++ app/src/main/java/com/takwolf/android/demo/loopviewpager/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.takwolf.android.demo.loopviewpager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.takwolf.android.demo.loopviewpager.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
